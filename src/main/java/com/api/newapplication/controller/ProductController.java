afff916c7858a5c7f24a3fec1c84f3ac827b6578
jesse-snow <jesse.neves@outlook.com>
2024-01-14 05:48:31 -0300

    Added Get route

--- src/main/java/com/api/newapplication/controller/ProductController.java
+++ src/main/java/com/api/newapplication/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.api.newapplication.controller;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,12 @@ public class ProductController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<List<ProductModel>> getProducts(){
+        return ResponseEntity
+                 .status(HttpStatus.OK)
+                 .body(productRepository.findAll());
     }
 }
